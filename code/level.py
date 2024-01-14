c3b8cd89b75a88ccf0b31e9780c1eeedc1ab7a2e
aajostar <80013602+aajostar@users.noreply.github.com>
2024-01-13 20:46:38 -0500

    add character status logic and animations, minor refactoring, and removal of old world map

--- code/level.py
+++ code/level.py
@@ -1,10 +1,10 @@
-import pygame
 from settings import *
 from tile import Tile
-from player import Player
 from debug import debug
-from support import *
+from player import Player
 from random import choice
+from support import *
+
 
 class Level:
     def __init__(self):
@@ -28,7 +28,6 @@ class Level:
             'grass': import_folder('../graphics/grass'),
             'objects': import_folder('../graphics/objects')
         }
-        print(graphics)
         for style, layout in layouts.items():
             for row_index, row in enumerate(layout):
                 for col_index, col in enumerate(row):
@@ -49,6 +48,7 @@ class Level:
         # update and draw the game
         self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
+        debug(self.player.status)
 
 
 class YSortCameraGroup(pygame.sprite.Group):
